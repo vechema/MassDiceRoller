62963f3aba434b4ed4c6cad6c0c1411972394322
Jacob Egner <jmegner@gmail.com>
2021-09-29 17:55:59 -0500

    update to Bootstrap 5
    which requires React-Boostrap 2-beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
     <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">
-          MassDiceRoller Skeleton
-        </h1>
-      </Jumbotron>
-      <h2>Section1</h2>
-      <h2>Section2</h2>
+      <h1 className="header">
+        MassDiceRoller Skeleton
+      </h1>
+      <div className="container">
+        <div className="row">
+        </div>
+      </div>
     </Container>
   );
 };
