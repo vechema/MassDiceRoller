f2617388911919b12381176fecb6e366434d7a94
Jacob Egner <jmegner@gmail.com>
2021-11-24 02:19:55 -0600

    add exact-result-table and reroll feature

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,36 @@ import * as _ from 'lodash';
 
 
 type AppState = {
+  rng: Random,
   diceQuantity: number,
   rolledDiceCounts: Map<number,number>,
-  rng: Random,
+  pipsToReroll: Set<number>,
+}
+
+class BaseComponent extends React.Component {
+  constructor(props: any, funcsToBind: Array<()=>void>) {
+    super(props);
+
+    for(let func of funcsToBind) {
+      this[func.name as keyof this] = (this[func.name as keyof this] as unknown as Function).bind(this);
+    }
+  }
 }
 
 export default class App extends React.Component {
   state: AppState = {
+    rng: new Random(MersenneTwister19937.autoSeed()),
     diceQuantity: 4,
     rolledDiceCounts: new Map<number,number>(),
-    rng: new Random(MersenneTwister19937.autoSeed()),
+    pipsToReroll: new Set<number>(),
   };
 
   constructor(props: any) {
     super(props);
     this.handleRollClick = this.handleRollClick.bind(this);
+    this.handleRerollClick = this.handleRerollClick.bind(this);
+    this.handleDiceQuantityChange = this.handleDiceQuantityChange.bind(this);
+    this.handlePipsToRerollChange = this.handlePipsToRerollChange.bind(this);
   }
 
   randomRollResult(rng: Random, diceQuantity: number): Map<number,number> {
@@ -38,6 +53,17 @@ export default class App extends React.Component {
     return rollResultCounts;
   }
 
+  combineRollResults(rolls1: Map<number,number>, rolls2: Map<number,number>) {
+    let pips = _.union(Array.from(rolls1.keys()), Array.from(rolls2.keys()));
+    let combinedRolls = new Map<number,number>();
+
+    for(let pip of pips) {
+      combinedRolls.set(pip, (rolls1.get(pip) ?? 0) + (rolls2.get(pip) ?? 0) );
+    }
+
+    return combinedRolls;
+  }
+
   handleDiceQuantityChange(event: any) {
     this.setState({diceQuantity: event.target.value});
   }
@@ -45,6 +71,37 @@ export default class App extends React.Component {
   handleRollClick() {
     this.setState((oldState: AppState) => ({
       rolledDiceCounts: this.randomRollResult(oldState.rng, oldState.diceQuantity)}));
+  }
+
+  handlePipsToRerollChange(pip: number, newValue: boolean) {
+    this.setState((oldState: AppState) => {
+      newValue ? oldState.pipsToReroll.add(pip) : oldState.pipsToReroll.delete(pip);
+      return { pipsToReroll: oldState.pipsToReroll };
+     });
+  }
+
+  handleRerollClick() {
+    let numDiceToReroll = 0;
+    let newRolledDiceCounts = new Map<number,number>();
+
+    // TODO: some central spot to have PipMin=1 and PipMax=6
+    for(let pip = 1; pip <= 6; pip++) {
+      let diceForThisPip = this.state.rolledDiceCounts.get(pip) ?? 0;
+
+      if(this.state.pipsToReroll.has(pip)) {
+        numDiceToReroll += diceForThisPip;
+        newRolledDiceCounts.set(pip, 0);
+      }
+      else {
+        newRolledDiceCounts.set(pip, diceForThisPip);
+      }
+    }
+
+    newRolledDiceCounts = this.combineRollResults(
+      newRolledDiceCounts,
+       this.randomRollResult(this.state.rng, numDiceToReroll));
+
+    this.setState({rolledDiceCounts: newRolledDiceCounts});
   }
 
   render() {
@@ -57,17 +114,42 @@ export default class App extends React.Component {
         </Jumbotron>
         <Row>
           <Col>
-            <input placeholder="Dice Quantity" value={this.state.diceQuantity} />
-            <Button className="m-2">Roll</Button>
+            <input placeholder="Dice Quantity" value={this.state.diceQuantity} onChange={this.handleDiceQuantityChange} />
+            <Button className="m-2" onClick={this.handleRollClick}>Roll</Button>
           </Col>
         </Row>
         <Row>
           <Col>
-            <ResultTable displayMin={1} displayMax={3} diceCounts={this.state.rolledDiceCounts} />
+            <ResultTable isExactResult={false} displayMin={1} displayMax={3} diceCounts={this.state.rolledDiceCounts} />
           </Col>
           <Col>
-            <ResultTable displayMin={4} displayMax={6} diceCounts={this.state.rolledDiceCounts} />
+            <ResultTable isExactResult={false} displayMin={4} displayMax={6} diceCounts={this.state.rolledDiceCounts} />
           </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ResultTable
+              isExactResult={true}
+              displayMin={1}
+              displayMax={3}
+              diceCounts={this.state.rolledDiceCounts}
+              pipsToReroll={this.state.pipsToReroll}
+              pipsToRerollChangeHandler={this.handlePipsToRerollChange}
+              />
+          </Col>
+          <Col>
+            <ResultTable
+              isExactResult={true}
+              displayMin={4}
+              displayMax={6}
+              diceCounts={this.state.rolledDiceCounts}
+              pipsToReroll={this.state.pipsToReroll}
+              pipsToRerollChangeHandler={this.handlePipsToRerollChange}
+              />
+          </Col>
+        </Row>
+        <Row>
+          <Button className="m-2" onClick={this.handleRerollClick}>Reroll</Button>
         </Row>
       </Container>
     );
--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -3,28 +3,53 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 
 interface Props {
+    isExactResult: boolean;
     displayMin: number;
     displayMax: number;
     diceCounts: Map<number,number>
+    pipsToReroll?: Set<number>,
+    pipsToRerollChangeHandler?: (pip: number, newValue: boolean) => void,
 }
 
 const ResultTable: React.FC<Props> = (props: Props) => {
   let rows: any[] = [];
   let runningSum = 0;
 
-  for (let i = 1; i <= props.displayMax; i++) {
-      runningSum += (props.diceCounts.get(i) || 0);
-      if(i >= props.displayMin) {
-        rows.push(<tr key={i}><td>{i}+</td><td>{runningSum}</td></tr>)
+  for (let pip = 6; pip >= props.displayMin; pip--) {
+      let diceCount = props.diceCounts.get(pip) || 0;
+      runningSum += diceCount;
+      let relevantQuantity = props.isExactResult ? diceCount : runningSum;
+      let pipSuffix = props.isExactResult ? "s" : "+";
+      let rerollIsChecked = props.pipsToReroll === undefined ? false : props.pipsToReroll.has(pip);
+      let onChangeHandler = () => {
+        if(props.pipsToRerollChangeHandler === undefined) {
+          return;
+        }
+
+        props.pipsToRerollChangeHandler(pip, !rerollIsChecked);
+      };
+      let rerollCheckBox
+        = props.isExactResult
+        ? <td><input type="checkbox" id={"reroll" + pip} checked={rerollIsChecked} onChange={onChangeHandler} /></td>
+        : <></>;
+
+      if(pip <= props.displayMax) {
+        rows.push(<tr key={pip}><td>{pip}{pipSuffix}</td><td>{relevantQuantity}</td>{rerollCheckBox}</tr>)
       }
   }
+
+  rows.reverse();
   
+  let quantityTypeName = props.isExactResult ? "Exact" : "Stat";
+  let rerollHeader = props.isExactResult ? <th>Reroll?</th> : <></>;
+
   return ( 
       <Table striped>
           <thead>
               <tr>
-                  <th>Stat</th>
+                  <th>{quantityTypeName}</th>
                   <th>Qty</th>
+                  {rerollHeader}
               </tr>
           </thead>
           <tbody>
